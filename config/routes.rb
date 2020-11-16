b993aa8d6ebc0dc75cce974c88beadf00fe9cdbd
harisp9631 <haris.pandzic1@g>
2020-11-16 22:12:48 +0100

    Initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :admins
+
+  authenticated :admin do
+    root to: "admin/home#index", as: :authenticated_root
+  end
+
+  root to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
